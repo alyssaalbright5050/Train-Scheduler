1764f0304217599fbd7b040beb925d4da7121b72
Alyssa Albright <alyssaalbright5050@gmail.com>
2019-11-08 20:31:08 -0600

    final push

--- javascript/app.js
+++ javascript/app.js
@@ -1,44 +1,85 @@
 $(document).ready(function() {
 
    
-    var firebaseConfig = {
+    var config = {
     apiKey: "AIzaSyDRxgL6LPEHG8vQtXWCvN4aNYoZTjfXYSg",
     authDomain: "train-scheduler-6b70c.firebaseapp.com",
     databaseURL: "https://train-scheduler-6b70c.firebaseio.com",
     projectId: "train-scheduler-6b70c",
     storageBucket: "train-scheduler-6b70c.appspot.com",
-    messagingSenderId: "1038556769243",
-    appId: "1:1038556769243:web:5d38da4bf194ede7bf55fe",
-    measurementId: "G-NJ42Z8W5RQ"
+    
   };
   
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  var time = new Date();
-  $('#currentTime').text('Current Time: ' + time);
-
-  var trainName = '';
-  var destination = '';
-  var trainTime = '';
-  var frequency = '';
+  firebase.initializeApp(config);
+  var database = firebase.database();
   
-  $("#submitinfo").on('click', function(event){
-      event.preventDefault();
-
-      $("#trainName").val().trim();
-      $("#destination").val().trim();
-      $("#trainTime").val().trim();
-      $("#frequency").val().trim();
-
-      database.ref().push( {
-          trainName: trainName,
-          destination: destination,
-          trainTime: trainTime,
-          frequency: frequency,
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+  var timeRN = new Date(); 
+  $("#currentTime").text("Current Time: " + timeRN);
+  
+  //user input variables
+  var trainName = "";
+  var destination = "";
+  var trainTime = "";
+  var frequency = 0;
+  
+  $("#submitInfo").on("click", function(event){
+    event.preventDefault();
+  
+    trainName = $("#TrainName").val().trim();
+    destination = $("#Destination").val().trim();
+    trainTime = $("#TrainTime").val().trim();
+    frequency = $("#Frequency").val().trim();
+    
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  
+  });
+  //only allows the user to clear train info input until session is over
+ 
+  $("#clearInfo").on("click", function(event){
+    event.preventDefault();
+    $("td").empty();
+    database.ref().set("/", null)
   })
+  
+  database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().frequency);
+  
+    var userTime = moment(childSnapshot.val().trainTime, "hh:mm");
+    //the time that the user feeds into input
+    var difference = moment().diff(moment(userTime), "minutes"); 
+    // calculating the time now (moment()) b/n the user's time in minutes
+    var timeRemaining = difference % childSnapshot.val().frequency;
+    //calculates how much time is left based on the time & how frequently it comes (in minutes)
+    var minsAway = childSnapshot.val().frequency - timeRemaining;
+    var nextTrain = moment().add(minsAway, "minutes")
+  
+    console.log(userTime);
+    console.log(difference);
+    console.log(timeRemaining);
+    console.log(minsAway);
+    console.log(nextTrain);
+  
+  
+    var addRow = $("<tr></tr>")
+    //adding each individual cell(td) to each row(tr)
+    addRow.append("<td>" + childSnapshot.val().trainName + "</td>")
+    .append("<td>" + childSnapshot.val().destination + "</td>")
+    .append("<td>" + childSnapshot.val().trainTime + "</td>")
+    .append("<td>" + childSnapshot.val().frequency + "</td>")
+    .append("<td>" + moment(nextTrain).format("hh:mm") + "</td>")
+    .append("<td>" + minsAway + "</td>");
+    $(".table").prepend(addRow);
+  });
+  });
 
 
 
@@ -48,5 +89,3 @@ $(document).ready(function() {
 
 
 
-
-}
